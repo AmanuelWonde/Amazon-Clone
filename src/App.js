11f75e54e2556eb3018bdd2eb3d0f9286e5cdd16
amanuelWonde <134140189+amanuelWonde@users.noreply.github.com>
2023-05-23 20:58:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Checkout from "./Components/CheckoutPage/Checkout";
+import Header from "./Components/Header/Header";
+import HomePageComponentWrapper from "./Components/HomePageComponentWrapper/HomePageComponentWrapper";
+import SigninPage from "./Components/SigninPage/SigninPage";
+import { useEffect, useState } from "react";
+import { auth } from "./Components/Firebase/Config";
+import { useStateValue } from "./Components/StateProvider/StateProvider";
+import Button from "./Components/NavMenu/Button";
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      // console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <HomePageComponentWrapper />{" "}
+              </>
+            }
+          />
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+          <Route path="signin" element={<SigninPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
